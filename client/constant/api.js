53a2898fb67d49f00ef4e4ea21f37417f153e540
quyentrankhanhha <t9qutr00@students.oamk.fi>
2022-05-29 22:09:24 +0300

    create new branch for server

--- client/constant/api.js
+++
@@ -1,3 +0,0 @@
-export const PIC_URL = 'http://localhost:8000/api/pic'
-export const LOGIN_URL = 'http://localhost:8000/api/login'
-export const REGISTER_URL = 'http://localhost:8000/api/register'
--- client/utils/index.js
+++
@@ -1,3 +0,0 @@
-export const base64ToDataUri = (base64) => {
-  return 'data:image/png;base64,' + base64
-}
